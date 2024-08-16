b2b18cf3e97dd82275dba236b0fa6b24741d8f90
JanniLund <202008456@post.au.dk>
2024-08-16 09:48:18 +0000

    gem

--- task2.py
+++ task2.py
@@ -1,19 +1,16 @@
 # 2. String Manipulation and Regular Expressions (20%)
 
 import re
-# reg = re.compile("([a-z][a-z]){2,5}@[f-p]{2,4}(\.company\.com)") 
-reg = re.compile("([a-z][a-z]){2,5} ") 
-code = reg.match('jdoe@hr.company.com')
-print(code)
+# Is based on the fact that you can have one or two intials in both first and last name
+reg = re.compile("([a-z][a-z]){1,5}@(hr|it|fin|mkt|ops)\.company\.com") 
 
-
-# def validate_email(email):
-#     while True:
-#         if reg.match(email):
-
-#             print(f"The inventory code '{email}' is valid.")
-#         else: 
-#             print(f"The inventory code '{emil}' is invalid.")
+def validate_email(email):
+    if reg.match('jdoe@hr.company.com'):
+        print(f"The email '{email}' is valid.")
+        return True
+    else: 
+        print(f"The email '{email}' is invalid.")
+        return False
 
 
 # def get_department(email):
@@ -21,7 +18,7 @@ print(code)
 #     codeDict = {match.group(3) for match in matches}
 #     return codeDict
 
-# email = ('jdoe@hr.company.com')
+email = ('jdoe@hr.company.com')
 
 # def categorize_emails(email_list):
 
